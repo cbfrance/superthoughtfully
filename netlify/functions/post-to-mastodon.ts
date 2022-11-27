4fe7d1205184888761e20219cf836d28437f9701
Christopher France <christopher@weavegrid.com>
2022-11-26 16:04:56 -0800

    Path

--- netlify/functions/post-to-mastodon.ts
+++ netlify/functions/post-to-mastodon.ts
@@ -1,4 +1,3 @@
-import fetch from "node-fetch";
 import fs from "fs";
 import { schedule } from "@netlify/functions";
 import path from "path";
@@ -9,7 +8,8 @@ import Mastodon from "mastodon";
 // Schedules the handler function to run at midnight on
 // Mondays, Wednesday, and Friday
 const handler = schedule("@hourly", async (event) => {
-  
+  console.log("------------------- Received event, posting ...");
+  console.log(event);
 
   const mastodon = new Mastodon({
     access_token: "gyG6CqeULjD5mkNS23fmzUPPxKIZbQMhKmS3H2wOMM4",
@@ -18,24 +18,21 @@ const handler = schedule("@hourly", async (event) => {
   });
 
   let id: string = "";
-  const imagesDir: string = "public/twitter-export";
+  const imagesDir: string = "dist/twitter-export";
   const baseDir = path.join(process.cwd(), imagesDir);
 
-  //   post a random images from public/twitter-export
+  //   post a random images from dist/twitter-export
   fs.readdir(imagesDir, (err, files) => {
     if (err) {
-      console.error(err)
-      return {
-        statusCode: 500,
-      };
+      console.log(err);
     } else {
       var random = Math.floor(Math.random() * files.length);
       var file = files[random];
 
-      const imagePath = path.join(baseDir, file)
+      const imagePath = path.join(baseDir, file);
 
-      console.log(imagePath)
-    
+      console.log(imagePath);
+
       mastodon
         .post("media", { file: fs.createReadStream(imagePath) })
         .then((resp) => {
